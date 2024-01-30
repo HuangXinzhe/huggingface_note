009bd1851eba6735aeb026706684dc4d8233098d
黄昕哲 <315680524@qq.com>
2024-01-30 17:49:07 +0800

    use transformers

--- 00_quick_start/08_translation.py
+++
@@ -1,9 +0,0 @@
-from transformers import pipeline
-
-translator = pipeline("translation", model="Helsinki-NLP/opus-mt-fr-en")  # 法语翻译英语
-# max_length或min_length控制翻译长短
-answer = translator("Ce cours est produit par Hugging Face.")
-print(answer)
-"""
-[{'translation_text': 'This course is produced by Hugging Face.'}]
-"""
